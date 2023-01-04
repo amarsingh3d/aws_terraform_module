0e0d4aad2f76d93ce3d9c324c10f687be3dd3116
root <root@TSCDELLAP1351.thesmartcube.net>
2023-01-04 19:30:45 +0530

    vpc module

---
+++ vpc/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = var.region
+
+}
+
+module "prod_vpc" {
+  region                       = var.region
+  source                       = "../modules/vpc"
+  vpc_name                     = var.vpc_name
+  cidr_block                   = var.cidr_block
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+
+}
